8b56b49ea85aca8514cab4b4a4af77ff00453ea8
ryxnh <349739326@gapps.yrdsb.ca>
2024-12-16 16:46:05 -0500

    added music and jumping sfx

--- fightStage.java
+++ fightStage.java
@@ -1,13 +1,32 @@
 import greenfoot.*;
+import greenfoot.GreenfootSound;
 
 public class fightStage extends World {
+    
+    private GreenfootSound backgroundMusic;
+    
     public fightStage()
     {
         super(600, 400, 1);
         
+        backgroundMusic = new GreenfootSound("stageOneMusic.mp3"); 
+        
         setBackground("stage_1.png");
         prepare(); 
     }
+    
+    public void stopped()
+    {
+        // pause music when the world stopped
+        backgroundMusic.pause();
+    }
+    
+    public void started()
+    {
+        // Resume music when the world started 
+        backgroundMusic.playLoop();
+    }
+    
     
     private void prepare()
     {
