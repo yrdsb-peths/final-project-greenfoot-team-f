a18c41da0c3f518df83c350a2d3962fc7f732b03
Nicholas-lh <350373023@gapps.yrdsb.ca>
2024-12-18 00:27:51 -0500

    Button created in instruction screen, now working on scaling

--- ScreenSelect.java
+++ ScreenSelect.java
@@ -17,10 +17,18 @@ public class ScreenSelect extends World
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1);
         
+        // Instruction button
         addObject(new Button(this::goInstructions, "InstructionButton.png", "InstructionButton.png"), 499,371);
+        
+        addObject(new Button(this:: goFightStage, "fightButton.png", "fightButton.png"), 499,100);
     }
     private void goInstructions()
     {
         Greenfoot.setWorld(new InstructionScreen());
+    }
+    
+    private void goFightStage()
+    {
+        Greenfoot.setWorld(new FightStage()); 
     }
 }
