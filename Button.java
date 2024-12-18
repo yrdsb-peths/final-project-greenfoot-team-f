7272dd4e6389b22b5cf65779bc64071bcd6cbd8d
scarlettho <159584626+scarlettho@users.noreply.github.com>
2024-12-18 12:29:56 -0500

    added hover effect to button

--- Button.java
+++ Button.java
@@ -12,10 +12,16 @@ public class Button extends Actor
     private GreenfootSound selectSound = new GreenfootSound("menuSelectSound2.mp3");
     private Runnable action; 
     private GreenfootImage buttonImage;
-    public Button(Runnable action, String imageName)
+    private GreenfootImage hoverImage;
+    
+    public Button(Runnable action, String imageName, String hoverName)
     {
         this.action = action;
         buttonImage = new GreenfootImage(imageName); //requirement when calling Button
+        hoverImage = new GreenfootImage(hoverName); 
+        
+        hoverImage.scale((int)(hoverImage.getWidth()*6/5), (int)(hoverImage.getHeight()*6/5));
+
         setImage(buttonImage);
     }
     public void act()
@@ -28,6 +34,14 @@ public class Button extends Actor
             {
                 action.run();
             }
+        }
+        if (Greenfoot.mouseMoved(this))
+        {
+            setImage(hoverImage); // Set hover image when mouse is over the button
+        } 
+        else if (Greenfoot.mouseMoved(null))
+        {
+            setImage(buttonImage); // Set normal image when mouse is not over the button
         }
     }
 }
