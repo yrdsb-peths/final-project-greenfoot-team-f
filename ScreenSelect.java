15a2b7b340bb65527ee2eb208da13f856b9a3396
scarlettho <159584626+scarlettho@users.noreply.github.com>
2025-01-06 12:26:31 -0500

    test

--- ScreenSelect.java
+++ ScreenSelect.java
@@ -16,7 +16,7 @@ public class ScreenSelect extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1);
-        
+        setBackground("instructionScreenBgBleach.png");
         // Instruction button
         addObject(new Button(this::goInstructions, "InstructionButton.png", "InstructionButton.png"), 499,371);
         
