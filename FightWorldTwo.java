d85bf9d47f76bfd93709c9c260341fe231e3b587
ryxnh <349739326@gapps.yrdsb.ca>
2025-01-13 10:34:54 -0500

    added ichigo class and fightworldtwo class

---
+++ FightWorldTwo.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+
+
+//ichigoFrame2
+
+public class FightWorldTwo extends World
+{
+
+    /**
+     * Constructor for objects of class FightWorldTwo.
+     * 
+     */
+    public FightWorldTwo()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+    }
+}
